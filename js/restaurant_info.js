ec1911335dfa26c3afdae38ed351703fbd3f5f79
Bill Tucker <bill@realgo.com>
2018-11-18 06:48:34 -0700

    add api key and fix line endings in main.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiaW1iaWxsdHVja2VyIiwiYSI6ImNpZXc1Y3h2MTB5cjRzcG0yOTNucTNybzYifQ.pMeWOrvU-11GqffBZ-BDWw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
